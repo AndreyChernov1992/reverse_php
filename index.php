06f7155cc81f290d4d64303cdc6d06d4018f0612
Coldsun <robotsdontcry92@gmail.com>
2023-05-18 22:19:39 +0700

    Done

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 use App\App\Reverse;
 
-$str = "olleH dlroW";
+$str = "abc1d";
 
 $reverse = new Reverse($str);
 
-$reverse->reverse($str);
+echo $reverse->reverse($str);
